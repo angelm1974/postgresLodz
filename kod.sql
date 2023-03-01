e0438b213cec0e0a9b81a415ae9f32a7c8598acc
angelm1974 <mariusz980@gmail.com>
2023-03-01 09:27:08 +0100

    Update kod.sql

--- kod.sql
+++ kod.sql
@@ -3,3 +3,6 @@ SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as u
 
 -- sprawdzenie wielkosci aktualnej bazy
 SELECT pg_database_size(current_database())
+
+-- ile zajmują miejsca wszystkie bazy danych
+SELECT sum(pg_database_size(datname))from pg_database
