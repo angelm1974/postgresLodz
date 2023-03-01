88c4632b398f78a673c73c2ef8a9e94f9caa8723
angelm1974 <mariusz980@gmail.com>
2023-03-01 09:23:38 +0100

    up2

--- kod.sql
+++ kod.sql
@@ -1,1 +1,5 @@
+-- sprawdzenie czasu uruchomienia serwera
 SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime;
+
+-- sprawdzenie wielkosci aktualnej bazy
+SELECT pg_database_size(current_database())
